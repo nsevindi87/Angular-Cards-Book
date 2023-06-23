a4b504b3cf5c7d82dfa9c3b1578eb30f819d82bb
Nizami Sevindi <nsevindi87@gmail.com>
2023-06-23 18:51:21 +0200

    angular project first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
+
+  //Kullanilan komponentler import edildikten sonra buraya tanimlanir
   declarations: [
     AppComponent
   ],
+
+
   imports: [
     BrowserModule
   ],
   providers: [],
+
+  //Program baslayinca ilk buraya bakar
   bootstrap: [AppComponent]
 })
 export class AppModule { }
