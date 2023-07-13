09d2156d686472164e9495e687e2acba197d738a
Nizami Sevindi <nsevindi87@gmail.com>
2023-07-13 23:50:54 +0200

    new angular App

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private service:OrderDetailsService){}
+
+  foodData: any;
+
+  ngOnInit(): void{
+    this.foodData = this.service.foodDetails;
+  }
 }
+
+/* VERI CEKMEK ICIN */
+/* 
+1- Servici import et
+2- classa ve importa OnInit implement et
+3- constructor icinde servisi degiskene ata
+4- foodData isimli degisken ata
+5- ngOnInit icinde foodData degiskenine servisteki arrayi ata
+ngOnInit component ilk baslatildiginda islem yapar
+*/
